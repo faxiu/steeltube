e9f99670aba1fa411f36ca2c42456ef79fd5cfc0
hekai <412097984@qq.com>
2018-11-26 11:05:16 +0800

    创建项目

---
+++ src/test/java/com/faxiu/steeltube/SteeltubeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.faxiu.steeltube;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SteeltubeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
