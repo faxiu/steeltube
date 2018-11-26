cf5cb77e3a8dd432c24eadaa42795778507c2279
hekai <412097984@qq.com>
2018-11-26 11:15:48 +0800

    创建项目，第一次测试提交

---
+++ src/main/java/com/faxiu/steeltube/dataobject/User.java
@@ -0,0 +1,22 @@
+package com.faxiu.steeltube.dataobject;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ * @Author hekai
+ * @Date 2018/11/26 11:13
+ */
+@Data
+@Entity
+public class User {
+
+    @Id
+    private String uuid;
+
+    private String username;
+
+    private String password;
+}
